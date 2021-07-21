39d5512c5001bbd521325e13570a160b7f462459
Ula Buczak <mankowskau@gmail.com>
2021-07-21 22:17:08 +0200

    Add basic sounds

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,41 +1,63 @@
 class DrumKit {
-    constructor(){
-        this.pads = document.querySelectorAll('.pad');
-        this.playButton = document.querySelector('.play');
-        this.kickAudio = document.querySelectorAll('.kick-sound');
-        this.clapSound = document.querySelector('.kick-sound');
-        this.crashSound = document.querySelector('.crash-sound');
-        this.index = 0;
-        this.bpm = 40;
-    }
+  constructor(){
+    this.pads = document.querySelectorAll('.pad');
+    this.playButton = document.querySelector('.play');
+    this.kickAudio = document.querySelector('.kick-sound');
+    this.clapAudio = document.querySelector('.clap-sound');
+    this.crashAudio = document.querySelector('.crash-sound');
+    this.index = 0;
+    this.bpm = 150;
+  }
 
+  activePad(){
+    this.classList.toggle('active');
+  }
 
-    activePad(){
-        this.classList.toggle('active');
-    }
+  repeat(){
+    let step = this.index % 8;
+    console.log(step);
+    const activeBars = document.querySelectorAll(`.b${step}`);
+    activeBars.forEach(bar=>{
+      bar.style.animation = 'playTrack 0.3s alternate ease-in-out 2';
+      //check if pads are active
+      if(bar.classList.contains('active')){
+        if (bar.classList.contains('kick-pad')){
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        }
+        if (bar.classList.contains('snare-pad')){
+          this.clapAudio.currentTime = 0;
+          this.clapAudio.play();
+        }
+        if (bar.classList.contains('crash-pad')){
+          this.crashAudio.currentTime = 0;
+          this.crashAudio.play();
+        }
+      }
+    });
+    this.index++;
+  }
 
-    repeat(){
-        let step = this.index % 8;
-        console.log(step);
-        const activeBars = document.querySelectorAll(`.b${step}`);
-        this.index++;
-    }
-
-    start(){
-        const interval = (60/this.bpm)*1000;
-        setInterval(() => {
-            this.repeat();
-        }, interval);
-    }
+  start(){
+    const interval = (60/this.bpm)*1000;
+    setInterval(() => {
+      this.repeat();
+    }, interval);
+  }
 }
 
 const drumKit = new DrumKit;
 
+console.log(drumKit);
+
 drumKit.pads.forEach(pad => {
-    pad.addEventListener('click', drumKit.activePad);
-})
+  pad.addEventListener('click', drumKit.activePad);
+  pad.addEventListener('animationend', function(){
+    this.style.animation = "";
+  });
+});
 
 drumKit.playButton.addEventListener('click', function(){
-    drumKit.start();
+  drumKit.start();
 });
 
